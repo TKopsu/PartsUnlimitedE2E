f4856e126af92f3a406c5c9cb7a2ebc7e6531a03
Kopsu <t.kopsu@gmail.com>
2022-03-03 13:01:04 +0200

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // Ensimmäinen muutostesti
+        // Toinen muutostesti
         [Key]
         public int CartItemId { get; set; }
 
